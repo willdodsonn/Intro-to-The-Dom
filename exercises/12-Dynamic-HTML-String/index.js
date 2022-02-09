785413f0ee7c51202bc9365d6add1c14759a9fdd
willdodsonn <sodlax38@gmail.com>
2022-02-09 04:43:09 +0000

    DOM day 2

--- exercises/12-Dynamic-HTML-String/index.js
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -1,2 +1,10 @@
-let myString = "<p>Hello!</p> <strong>My friend</strong>";
+let myString = "<p>Hello!</p> <strong>My friend,</strong><p>we are in the year</p>";
+// const d = new Date('2, 8, 22');
+// let year = d.getFullYear();
+
 document.write(myString);
+let elem = document.createElement("p");
+
+elem.myString.innerHTML = new Date().getFullYear();
+
+document.body.appendChild(elem);
