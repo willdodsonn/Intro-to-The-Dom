b3efa70b2253c22bb9e6efb0742ba1563507e328
willdodsonn <sodlax38@gmail.com>
2022-02-09 02:05:21 +0000

    DOM day 2

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,9 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	let aux = document.querySelector('#myList');
-	let elem = document.createElement("li");
-	elem.innerHTML = "Fourth Element";
-	document.aux.appendChild(elem);
+	 var ul = document.querySelector("#myList");
+  var li = document.createElement("li");
+  li.innerHTML = "Fourth Element";
+  ul.appendChild(li);
 
 });
