eab08e1f38cc40bfe94f04565db57500c2297a11
willdodsonn <sodlax38@gmail.com>
2022-02-09 00:03:53 +0000

    DOM day 2

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	let aux = document.querySelector('#myList');
+	let elem = document.createElement("li");
+	elem.innerHTML = "Fourth Element";
+	document.aux.appendChild(elem);
 
 });
