2727812c61eda48b741d04174a5b168f42f4f7ee
willdodsonn <sodlax38@gmail.com>
2022-02-10 23:55:00 +0000

    DOM day 4

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,43 +1,27 @@
-// Your code here
 
-
-
-//create element
-// let elem = document.createElement("p");
-// elem.innerHTML = "Hello World";
-// elem.style.background ="yellow";
-// document.body.appendChild(elem);
-
-
-//add li onclick
-// let button = document.querySelector("#addToDo");
-// button.addEventListener("click", function() {
-// 	 var ul = document.querySelector("#myList");
-//   var li = document.createElement("li");
-//   li.innerHTML = "Fourth Element";
-//   ul.appendChild(li);
-
-// });
-
-//remove element
+//remove event
 // let element = document.querySelector("#secondElement");
 // element.parentNode.removeChild(element);
 
-//change eventlistener 
+//add event
+
 let input = document.querySelector("input")
 input.addEventListener('change', function () {
     var ul = document.querySelector("ul");
       var li = document.createElement("li");
       var span = document.querySelector("span");
-      li.innerHTML=  input.value;
+      var clone = span.cloneNode(true);
+      var text = document.createTextNode(input.value);
+      li.appendChild(clone);
+      li.appendChild(text);
       ul.appendChild(li);
-      li.appendChild(span);
+    
     });
     
-    //  <span><i class=\"fa fa-trash\"></i></span>
-   
-    //   var span = document.createElement("span");
-    //   var span = li.appendChild("span");
-    //   var i = span.appendChild("i");
-    //   i.setAttribute("class", "fa fa-trash");
-    //   i.appendChild("li")
+  
+ let button = document.querySelector(".fa")   
+ button.addEventListener('click', function(clickedButton) {
+  //  var li = clickedButton.currentTarget.parentNode.parentNode;
+   var li = document.querySelector("li");
+   li.parentNode.removeChild(li); 
+ });
