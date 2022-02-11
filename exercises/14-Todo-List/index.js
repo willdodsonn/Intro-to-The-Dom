2c32a28c6bc7f0dfa0d0cd8004a87fb3c5bf760d
willdodsonn <sodlax38@gmail.com>
2022-02-11 15:30:40 +0000

    finished

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -11,6 +11,11 @@ input.addEventListener('change', function () {
       var li = document.createElement("li");
       var span = document.querySelector("span");
       var clone = span.cloneNode(true);
+      clone.addEventListener("click", function(e) {
+        var li = e.currentTarget.parentNode.parentNode;
+        //  var li = document.querySelector("li");
+         li.parentNode.removeChild(li); 
+        });
       var text = document.createTextNode(input.value);
       li.appendChild(clone);
       li.appendChild(text);
@@ -19,9 +24,18 @@ input.addEventListener('change', function () {
     });
     
   
- let button = document.querySelector(".fa")   
- button.addEventListener('click', function(clickedButton) {
-  //  var li = clickedButton.currentTarget.parentNode.parentNode;
-   var li = document.querySelector("li");
+
+
+document.querySelectorAll(".fa").forEach(function(clickedButton){
+clickedButton.addEventListener("click", function(e) {
+  var li = e.currentTarget.parentNode.parentNode;
+  //  var li = document.querySelector("li");
    li.parentNode.removeChild(li); 
- });
+  })
+});
+//   let button = document.querySelector(".fa")   
+//  button.addEventListener('click', function(clickedButton) {
+//    var li = clickedButton.currentTarget.parentNode.parentNode;
+//   //  var li = document.querySelector("li");
+//    li.parentNode.removeChild(li); 
+//  });
